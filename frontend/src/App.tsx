736c510cedc058e1aa615f935577fccca6349488
Aaron Sim <aaron.sim.359@gmail.com>
2025-01-07 18:11:59 +0900

    Clean up unused code

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Reset from "./components/Reset";
@@ -8,20 +8,6 @@ import Menu from "./components/Menu"
 const App: React.FC = () => {
   return (
       <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Login</Link>
-            </li>
-            <li>
-              <Link to="/register">Register</Link>
-            </li>
-            <li>
-              <Link to="/reset">Reset Password</Link>
-            </li>
-          </ul>
-        </nav>
-
         <Routes>
           <Route path="/" element={<Login />} />
           <Route path="/register" element={<Register />} />
