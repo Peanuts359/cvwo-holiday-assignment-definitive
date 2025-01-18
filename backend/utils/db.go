907d37aa2a749f749405b0589cb034b8f8128092
DESKTOP-Q4FL1FS\Admin <aaron.sim.359@gmail.com>
2025-01-18 17:50:30 +0800

    Implement backend database logic (for getting the database's instance)

---
+++ backend/utils/db.go
@@ -0,0 +1,28 @@
+package utils
+
+import (
+	"database/sql"
+	"log"
+	"sync"
+
+	_ "modernc.org/sqlite"
+)
+
+var (
+	db   *sql.DB
+	once sync.Once
+)
+
+func GetDB() *sql.DB {
+	once.Do(func() {
+		var err error
+		db, err = sql.Open("sqlite3", "./database.db")
+		if err != nil {
+			log.Fatalf("Failed to connect to the database: %v", err)
+		}
+
+		log.Println("Database connection established successfully")
+	})
+
+	return db
+}
